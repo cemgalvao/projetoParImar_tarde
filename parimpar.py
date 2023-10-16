239f2112e18e4f3e244f8dabd2227fbb2b36937a
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-10-16 14:31:28 -0300

    commit inicial Galvão

---
+++ parimpar.py
@@ -0,0 +1,20 @@
+# Projeto Par ou Impar
+
+# Apresentação
+
+print('\n\t\t\t -- verificador de números \n\n')
+
+# entradas
+num = int(input('informe um número:'))
+
+#processamento e saíde:
+
+if num%2==0:
+    print(f'\n {num} é número é par')
+else:
+    print(f'\n {num} é número é impar')
+
+
+
+
+
